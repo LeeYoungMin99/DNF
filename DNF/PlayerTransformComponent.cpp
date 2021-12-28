a20a38c01b4c37a99aacc059872d730845f31475
LeeYM <sixk1999@gmail.com>
2021-12-28 14:39:55 +0900

    fix : PlayerTransformComponent
    방향전환시 키입력이 제대로 처리되지 않아 방향전환이 안되던 버그 수정.

--- DNF/PlayerTransformComponent.cpp
+++ DNF/PlayerTransformComponent.cpp
@@ -17,12 +17,11 @@ void PlayerTransformComponent::Update()
 	case PlayerStatusComponent::ePlayerState::Idle:
 	case PlayerStatusComponent::ePlayerState::Walk:
 	case PlayerStatusComponent::ePlayerState::Run:
+		if		(Input::GetButtonDown(VK_LEFT))		{ SetDirX(eDirX::Left); }		
+		else if (Input::GetButtonDown(VK_RIGHT)) 	{ SetDirX(eDirX::Right); }
 
-		if		(Input::GetButtonDown(VK_LEFT))	 { SetDirX(eDirX::Left); }
-		else if (Input::GetButtonDown(VK_RIGHT)) { SetDirX(eDirX::Right); }
-
-		if (Input::GetButtonDown(VK_UP))	{ SetDirY(eDirY::Up); }
-		if (Input::GetButtonDown(VK_DOWN))	{ SetDirY(eDirY::Down); }
+		if		(Input::GetButtonDown(VK_UP))	{ SetDirY(eDirY::Up); }
+		else if (Input::GetButtonDown(VK_DOWN))	{ SetDirY(eDirY::Down); }
 
 		if		(Input::GetButtonUp(VK_RIGHT) && Input::GetButton(VK_LEFT)) { SetDirX(eDirX::Left); }
 		else if (Input::GetButtonUp(VK_LEFT) && Input::GetButton(VK_RIGHT)) { SetDirX(eDirX::Right); }
@@ -30,11 +29,12 @@ void PlayerTransformComponent::Update()
 		if		(Input::GetButtonUp(VK_DOWN) && Input::GetButton(VK_UP)) { SetDirY(eDirY::Up); }
 		else if (Input::GetButtonUp(VK_UP) && Input::GetButton(VK_DOWN)) { SetDirY(eDirY::Down); }
 
-		if		(eDirX::Left != _dirX)	{ if (Input::GetButton(VK_LEFT) && false == Input::GetButton(VK_RIGHT)) { SetDirX(eDirX::Left); } }
-		else if (eDirX::Right != _dirX) { if (Input::GetButton(VK_RIGHT) && false == Input::GetButton(VK_LEFT)) { SetDirX(eDirX::Right); } }
+		if		(eDirX::Right == _dirX)	{ if (Input::GetButton(VK_LEFT) && false == (Input::GetButton(VK_RIGHT) || Input::GetButtonDown(VK_RIGHT))) { SetDirX(eDirX::Left); } }
+		else if (eDirX::Left == _dirX) { if (Input::GetButton(VK_RIGHT) && false == (Input::GetButton(VK_LEFT) || Input::GetButtonDown(VK_LEFT))) { SetDirX(eDirX::Right); } }
 
-		if		(eDirY::Up != _dirY)	{ if (Input::GetButton(VK_UP) && false == Input::GetButton(VK_DOWN)) { SetDirY(eDirY::Up); } }
-		else if (eDirY::Down != _dirY)	{ if (Input::GetButton(VK_DOWN) && false == Input::GetButton(VK_UP)) { SetDirY(eDirY::Down); } }
+		if		(eDirY::Down == _dirY)	{ if (Input::GetButton(VK_UP) && false == (Input::GetButton(VK_DOWN) || Input::GetButtonDown(VK_DOWN))) { SetDirY(eDirY::Up); } }
+		else if (eDirY::Up == _dirY)	{ if (Input::GetButton(VK_DOWN) && false == (Input::GetButton(VK_UP) || Input::GetButtonDown(VK_UP))) { SetDirY(eDirY::Down); } }
+
 		break;
 
 	case PlayerStatusComponent::ePlayerState::Jump:
